00593250066cfc9257c9b64d3561c9fa44549248
Smoqee <kaan.private@outlook.de>
2022-08-25 20:22:19 +0200

    Set basic structure

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import './styles/style.css'
+console.log('hello there')
+
+let todoItems = []
+let projectItems = []
---
+++ src/scripts/todo.js
@@ -0,0 +1,12 @@
+class TodoItem {
+    constructor(title, description, dueDate, priority) {
+        this.title = title
+        this.description = description
+        this.dueDate = dueDate
+        this.priority = priority
+    }
+}
+
+const todoCreation = () => {
+    
+}
